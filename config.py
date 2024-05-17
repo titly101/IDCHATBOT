3443c05a91f34f8a092a841758e14dbf74540710
titly101 <titlybanerjee302@yahoo.com>
2024-05-17 07:34:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,27 +5,27 @@ load_dotenv()
 
 
 ### ❖ ➥
-API_ID = int(getenv("API_ID", None))
+API_ID = int(getenv("API_ID", "27293061"))
 
 ### ❖ ➥
-API_HASH = getenv("API_HASH", None)
+API_HASH = getenv("API_HASH", "3d94fbc0ba33ef252fccd29bdeb14a39")
 
 ### ❖ ➥
-STRING_SESSION = getenv("STRING_SESSION", None)
+STRING_SESSION = getenv("STRING_SESSION", "BQGgdYUAxnRqrA514EO-Kgc06mqNgoJ802z-91aktwq6DDjBPOd8BsBsv1cRB4gpWCw59QOOYWWgaRSQZoPD6NNOrw3r9Jrd1RSpD6n5gU_yESUtjpZlBiSIWzFShV1d3DgKVZ3bIiHihdJduCSRvGXgNbhZHWothJqs0Rse-wODNal_iXoE96OQFvIXvgSVst1oQTrqhjw-T_YRCNAZp2Tk3XLRTUE5s1-dojdUaHp5tUVJExfe6d3Zw-lsMQWoh8frG0USE0dXN0xOpDDaIJJ55SpVhcpNTNGcSsM5bXOm9qkEWINL5tr1DZpZ46fl2uep-N0LB9V41Zxnwfjtuk1N0uLXdgAAAAGokPMNAA")
 
 ### ❖ ➥
-OWNER_ID = int(getenv("OWNER_ID", "6922271843"))
+OWNER_ID = int(getenv("OWNER_ID", "7123038989"))
 
 ### ❖ ➥
-MONGO_URL = getenv("MONGO_URL", None)
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority")
 
 ### ❖ ➥
-SUPPORT_GRP = getenv("SUPPORT_GRP", "THE_FRIENDZ")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "https://t.me/abhjkfv")
 
 ### ❖ ➥
-UPDATE_CHNL = getenv("UPDATE_CHNL", "ROY_EDITX")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "https://t.me/Movie_FlixChannel")
 
 ### ❖ ➥
-OWNER_USERNAME = getenv("OWNER_USERNAME", "LOCO_X_PILOT")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "https://t.me/AJJU_UUH99")
 
 
